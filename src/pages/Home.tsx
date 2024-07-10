8436d91b57ee2b21e138e85fcfed28434c9e2b55
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-10 14:25:30 +0100

    chore: clean-up

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,5 @@
 import { Suspense } from "react";
-import {
-  IonContent,
-  IonHeader,
-  IonPage,
-  IonTitle,
-  IonToolbar,
-} from "@ionic/react";
+import { IonContent, IonPage } from "@ionic/react";
 
 import { Main } from "../components/Main";
 
@@ -13,12 +7,7 @@ export function Home() {
   return (
     <IonPage>
       <IonContent fullscreen={true}>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <Suspense>
+        <Suspense fallback={null}>
           <Main />
         </Suspense>
       </IonContent>
