01f206a6d6fb79e14a1d15aa9597506d10368682
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-12 16:23:12 +0100

    feat(wip|search): add `search verses/chapters` logic

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,34 +1,79 @@
-import { useState } from "react";
+import { IonSearchbar } from "@ionic/react";
+import { useEffect, useState } from "react";
+
+type Surah = {
+  id: number;
+  name: string;
+  name_en: string;
+  name_translation: string;
+  words: number;
+  letters: number;
+  type: string;
+  type_en: string;
+  ar: string;
+  en: string;
+  array: Array<{
+    id: number;
+    ar: string;
+    en: string;
+    filename: string;
+    path: string;
+    dir: string;
+    size: number;
+  }>;
+};
 
 export function Search() {
   const [searchQuery, setSearchQuery] = useState("");
-  const [searchResults, setSearchResults] = useState<number[]>([]);
+  const [searchResults, setSearchResults] = useState<Surah[]>([]);
+  const [data, setData] = useState<Surah[]>([]);
 
-  function setCurrentPage(page: number) {
+  function goToPage(page: number) {
     // @todo
     console.log({ page });
   }
 
-  function handleSearch() {
-    // @todo
-    setSearchResults([]);
-  }
+  useEffect(() => {
+    function getQuranData() {
+      fetch("./quran.json")
+        .then((response) => response.json())
+        .then((rawData) => {
+          setData(rawData);
+        })
+        .catch((error) => console.log(error.message));
+    }
+    getQuranData();
+  }, []);
+
+  useEffect(() => {
+    if (searchQuery !== "") {
+      const filteredData = data.filter((item) => {
+        return (
+          item.name.includes(searchQuery) ||
+          item.ar.includes(searchQuery) ||
+          item.array.some(({ ar: verse }) => verse.includes(searchQuery))
+        );
+      });
+
+      setSearchResults(filteredData);
+      console.log({ filteredData, searchQuery });
+    }
+  }, [searchQuery]);
 
   return (
-    <section style={{ display: "none" }}>
-      <input
-        type="text"
+    <section>
+      <IonSearchbar
+        animated={true}
         value={searchQuery}
-        onChange={(e) => setSearchQuery(e.target.value)}
-        placeholder="Search..."
-      />
-      <button onClick={handleSearch}>Search</button>
+        placeholder="بحث"
+        onIonInput={(event) => setSearchQuery(event.target.value!)}
+      ></IonSearchbar>
       <div>
         <h4>Search Results</h4>
         <ul>
           {searchResults.map((page) => (
-            <li key={page} onClick={() => setCurrentPage(page)}>
-              Page {page + 1}
+            <li key={page.id} onClick={() => goToPage(page.id)}>
+              Page {page.id + 1}
             </li>
           ))}
         </ul>
