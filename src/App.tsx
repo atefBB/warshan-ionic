2a46c0b9bfb6d237da2d9b2c09c1ab9b098fcaa0
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-17 09:33:03 +0100

    chore: clean-up

--- src/App.tsx
+++ src/App.tsx
@@ -17,8 +17,6 @@ import "@ionic/react/css/text-transformation.css";
 import "@ionic/react/css/flex-utils.css";
 import "@ionic/react/css/display.css";
 
-// import "@ionic/react/css/palettes/dark.system.css";
-
 import "./theme/variables.css";
 
 setupIonicReact();
