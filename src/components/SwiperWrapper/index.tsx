4b4b5bd896b31c9f1a46717ec92c432fcc6c47f9
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-18 17:27:18 +0100

    feat: add `swiper wrapper` component

---
+++ src/components/SwiperWrapper/index.tsx
@@ -0,0 +1,62 @@
+import { useEffect, useRef, useState } from "react";
+import { IonContent, IonImg } from "@ionic/react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { useSnapshot } from "valtio";
+import { Swiper as SwiperInterface } from "swiper";
+
+import { setCurrentPage, store } from "../../store";
+
+import "swiper/css";
+import "@ionic/react/css/ionic-swiper.css";
+
+export function SwiperWrapper() {
+  const { currentPage, pages } = useSnapshot(store);
+
+  const swiperInstance = useRef<SwiperInterface>();
+
+  useEffect(() => {
+    if (swiperInstance !== undefined && currentPage - 1 !== 0) {
+      swiperInstance?.current?.slideTo(currentPage - 1);
+    }
+  }, [swiperInstance]);
+
+  function goToNextPage() {
+    if (currentPage < pages.length) {
+      setCurrentPage(currentPage + 1);
+    }
+  }
+
+  function goToPreviousPage() {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  }
+
+  return (
+    <IonContent className="content">
+      <Swiper
+        onSwiper={(swiper) => {
+          swiperInstance.current = swiper;
+        }}
+        dir="rtl"
+        style={{ height: "100%" }}
+        onSlideNextTransitionEnd={() => goToNextPage()}
+        onSlidePrevTransitionEnd={() => goToPreviousPage()}
+      >
+        {pages.map((page) => (
+          <SwiperSlide key={page.imageUrl}>
+            <IonImg
+              src={page.imageUrl}
+              alt={`الصفحة ${currentPage}`}
+              style={{
+                width: "100%",
+                height: "100%",
+                objectFit: "fill",
+              }}
+            />
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </IonContent>
+  );
+}
