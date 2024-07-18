f63e19f7011adf1502e7fc56f2093c87d9fe7370
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-18 09:27:37 +0100

    feat(Main): add `go fullscreen on click` feature

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,17 @@
+import { AndroidFullScreen } from "@awesome-cordova-plugins/android-full-screen";
 import { lazy } from "react";
 
 const Page = lazy(() => import("./Page"));
 
 export function Main() {
+  function goFullscreen() {
+    AndroidFullScreen.isImmersiveModeSupported()
+      .then(() => AndroidFullScreen.immersiveMode())
+      .catch(console.warn);
+  }
+
   return (
-    <main>
+    <main onClick={() => goFullscreen()}>
       <Page />
     </main>
   );
