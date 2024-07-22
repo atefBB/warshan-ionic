717ebfa3eb5977a058f39464b9c7210cdcbfb220
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-22 15:06:31 +0100

    refactor(header): review `logic`

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,35 +3,22 @@ import {
   IonToolbar,
   IonTitle,
   IonButtons,
-  IonButton,
   IonIcon,
-  ModalOptions,
 } from "@ionic/react";
 import { searchOutline, bookOutline } from "ionicons/icons";
-import { HookOverlayOptions } from "@ionic/react/dist/types/hooks/HookOverlayOptions";
 import {
   getSuraByPageNumber,
   getJuzByPageNumber,
   // @ts-ignore
 } from "@kmaslesa/quran-metadata";
 import { useSnapshot } from "valtio";
+import { Link } from "react-router-dom";
 
 import { store } from "../../store";
 
 import "./styles.css";
 
-type PropsType = {
-  openIndexModal: (
-    options?: Omit<ModalOptions, "component" | "componentProps"> &
-      HookOverlayOptions
-  ) => void;
-  openSearchModal: (
-    options?: Omit<ModalOptions, "component" | "componentProps"> &
-      HookOverlayOptions
-  ) => void;
-};
-
-export function Header({ openIndexModal, openSearchModal }: PropsType) {
+export function Header() {
   const { currentPage } = useSnapshot(store);
 
   const chapterName: Array<Record<string, any>> =
@@ -42,18 +29,14 @@ export function Header({ openIndexModal, openSearchModal }: PropsType) {
     <IonHeader style={{ direction: "rtl" }} className="ion-no-border">
       <IonToolbar>
         <IonButtons slot="start">
-          <IonIcon
-            slot="icon-only"
-            icon={bookOutline}
-            onClick={() => openIndexModal()}
-          />
+          <Link to="/chapters-index" style={{ color: "black" }}>
+            <IonIcon slot="icon-only" icon={bookOutline} />
+          </Link>
         </IonButtons>
         <IonButtons slot="end">
-          <IonIcon
-            onClick={() => openSearchModal()}
-            slot="icon-only"
-            icon={searchOutline}
-          />
+          <Link to="/search" style={{ color: "black" }}>
+            <IonIcon slot="icon-only" icon={searchOutline} />
+          </Link>
         </IonButtons>
 
         <IonTitle className="ion-text-center">
