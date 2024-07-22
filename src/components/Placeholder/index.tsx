a32cd0a60d1c08417910fcbbb393b719a7cf90c2
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-22 15:18:24 +0100

    feat(components): add `placeholder` component

---
+++ src/components/Placeholder/index.tsx
@@ -0,0 +1,16 @@
+export function Placeholder() {
+  return (
+    <strong
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100%",
+        background: "#00000066",
+        color: "white",
+      }}
+    >
+      اذكر الله
+    </strong>
+  );
+}
