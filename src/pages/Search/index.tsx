d00819b621b0eca29b74f19f42f0ac4287ec2969
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-25 17:06:39 +0100

    refactor(search): review `logic`

--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,61 +1,24 @@
 import { IonHeader, IonPage, IonToolbar } from "@ionic/react";
 import { IonContent, IonItem, IonSearchbar } from "@ionic/react";
-import { useEffect, useState } from "react";
-import { ArabicServices as arSrv } from "arabic-services";
-// @ts-ignore
-import { getSuraByIndex } from "@kmaslesa/quran-metadata";
+import { useState } from "react";
 import { useSnapshot } from "valtio";
 
 import { setCurrentPage, store } from "../../store";
 
-import "./styles.css";
+import { Result, searchVerses } from "../../helpers";
 
-type Surah = {
-  id: number;
-  name: string;
-  name_en: string;
-  name_translation: string;
-  words: number;
-  letters: number;
-  type: string;
-  type_en: string;
-  ar: string;
-  en: string;
-  array: Array<{
-    id: number;
-    ar: string;
-    en: string;
-    filename: string;
-    path: string;
-    dir: string;
-    size: number;
-  }>;
-};
+import "./styles.css";
 
 export function Search({ history }: any) {
   const { currentPage } = useSnapshot(store);
 
-  const [searchQuery, setSearchQuery] = useState<string>("");
-  const [searchResults, setSearchResults] = useState<Surah[]>([]);
-  const [data, setData] = useState<Surah[]>([]);
+  const [searchResults, setSearchResults] = useState<Result[]>([]);
   const [isSearchNumber, setIsSearchNumber] = useState<boolean>(false);
 
   function goToPage(page: number) {
     setCurrentPage(page);
     history.push("/");
   }
-
-  useEffect(() => {
-    function getQuranData() {
-      fetch("./quran.json")
-        .then((response) => response.json())
-        .then((rawData) => {
-          setData(rawData);
-        })
-        .catch((error) => console.log(error.message));
-    }
-    getQuranData();
-  }, []);
 
   function search(value: string) {
     if (value !== "") {
@@ -65,18 +28,8 @@ export function Search({ history }: any) {
       if (isValueNumber) {
         setCurrentPage(Number(value));
       } else {
-        return;
-        setSearchQuery(value);
-        const filteredData = data.filter((item) => {
-          return (
-            arSrv.removeTashkeel(item.name).includes(value) ||
-            arSrv.removeTashkeel(item.ar).includes(value) ||
-            item.array.some(({ ar: verse }) =>
-              arSrv.removeTashkeel(verse).includes(value)
-            )
-          );
-        });
-        setSearchResults(filteredData);
+        const results = searchVerses(value);
+        setSearchResults(results);
       }
     }
   }
@@ -89,7 +42,7 @@ export function Search({ history }: any) {
             class="custom-search-bar"
             animated={true}
             placeholder="بحث"
-            onIonInput={(event) => search(event.target.value!)}
+            onIonChange={(event) => search(event.target.value!)}
             onIonClear={() => {
               setIsSearchNumber(false);
               setSearchResults([]);
@@ -99,24 +52,16 @@ export function Search({ history }: any) {
       </IonHeader>
       <IonContent className="ion-padding">
         {searchResults.length > 0
-          ? searchResults.map((page) => {
-              const chapter = getSuraByIndex(page.id);
-              const verses = page.array.filter((verse) => {
-                const verseWithoutTashkeel = arSrv.removeTashkeel(verse.ar);
-                const isSearchedVerse =
-                  verseWithoutTashkeel.includes(searchQuery);
-                return isSearchedVerse;
-              });
-
-              return verses.map((searchedVerse) => (
+          ? searchResults.map(({ surah, verse }) => {
+              return (
                 <IonItem
-                  key={searchedVerse.id}
-                  onClick={() => goToPage(chapter.startPage)}
+                  key={`${surah.id}-${verse.number}`}
+                  onClick={() => goToPage(verse.page)}
                 >
                   {"سورة "}
-                  {page.name}: {searchedVerse.ar}
+                  {surah.name}: {verse.text}
                 </IonItem>
-              ));
+              );
             })
           : null}
         {isSearchNumber === true ? (
