ad4d8eac910e785736ffb62320cbfec219ac7fe1
Atef Ben Ali <atef.bettaib@gmail.com>
2024-07-30 17:12:45 +0100

    build(viteConfig): config `vite pwa`

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,16 @@
 /// <reference types="vitest" />
 
-import legacy from '@vitejs/plugin-legacy'
-import react from '@vitejs/plugin-react'
-import { defineConfig } from 'vite'
+import legacy from "@vitejs/plugin-legacy";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    legacy()
-  ],
+  plugins: [react(), legacy(), VitePWA({ registerType: "autoUpdate" })],
   test: {
     globals: true,
-    environment: 'jsdom',
-    setupFiles: './src/setupTests.ts',
-  }
-})
+    environment: "jsdom",
+    setupFiles: "./src/setupTests.ts",
+  },
+});
