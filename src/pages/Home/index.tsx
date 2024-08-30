f8f981f3b186d3acaf5c127870e1a56870c06c67
Atef Ben Ali <atef.bettaib@gmail.com>
2024-08-30 14:32:22 +0100

    feat(home): add `floating button`

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,18 +1,27 @@
 import { lazy, Suspense, useEffect } from "react";
 import { useSnapshot } from "valtio";
-import { IonPage, IonContent } from "@ionic/react";
+import {
+  IonFabList,
+  IonFabButton,
+  IonFab,
+  IonPage,
+  IonContent,
+  IonIcon,
+} from "@ionic/react";
+import { chevronUpCircle, searchOutline, listOutline } from "ionicons/icons";
 
 import { store } from "../../store";
 
 import { SpinnerWrapper } from "../../components";
 
 import "./grid.css";
+import "./style.css";
 
 const LazyCarouselWrapper = lazy(
   () => import("../../components/CarouselWrapper")
 );
 
-export function Home() {
+export function Home({ history }: any) {
   const { currentPage } = useSnapshot(store);
 
   useEffect(() => {
@@ -22,6 +31,19 @@ export function Home() {
   return (
     <IonPage>
       <IonContent>
+        <IonFab edge={true} slot="fixed" vertical="bottom" horizontal="end">
+          <IonFabButton color="dark" size="small">
+            <IonIcon icon={chevronUpCircle} />
+          </IonFabButton>
+          <IonFabList side="top">
+            <IonFabButton onClick={() => history.push("/chapters-index")}>
+              <IonIcon icon={listOutline} />
+            </IonFabButton>
+            <IonFabButton onClick={() => history.push("/search")}>
+              <IonIcon icon={searchOutline} />
+            </IonFabButton>
+          </IonFabList>
+        </IonFab>
         <Suspense fallback={<SpinnerWrapper />}>
           <LazyCarouselWrapper />
         </Suspense>
