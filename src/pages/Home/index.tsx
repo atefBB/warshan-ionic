d2a2278940d7aaca85a5f7131245d07fca3e66a9
Atef Ben Ali <atef.bettaib@gmail.com>
2024-08-30 15:05:28 +0100

    feat(home): show `floating button` on `double click` event

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,4 +1,4 @@
-import { lazy, Suspense, useEffect } from "react";
+import { lazy, Suspense, useEffect, useRef, useState } from "react";
 import { useSnapshot } from "valtio";
 import {
   IonFabList,
@@ -7,6 +7,7 @@ import {
   IonPage,
   IonContent,
   IonIcon,
+  createGesture,
 } from "@ionic/react";
 import { chevronUpCircle, searchOutline, listOutline } from "ionicons/icons";
 
@@ -22,28 +23,62 @@ const LazyCarouselWrapper = lazy(
 );
 
 export function Home({ history }: any) {
+  const DOUBLE_CLICK_THRESHOLD = 500;
+
   const { currentPage } = useSnapshot(store);
+  const [isFabButtonShown, setIsFabButtonShown] = useState<boolean>(false);
+  const content = useRef<HTMLIonContentElement | null>(null);
+
+  let lastOnStart = 0;
+
+  const onStart = () => {
+    const now = Date.now();
+
+    if (Math.abs(now - lastOnStart) <= DOUBLE_CLICK_THRESHOLD) {
+      if (content.current) {
+        setIsFabButtonShown((prevState) => !prevState);
+        lastOnStart = 0;
+      }
+    } else {
+      lastOnStart = now;
+    }
+  };
 
   useEffect(() => {
     localStorage.setItem("currentPage", `${currentPage}`);
   }, [currentPage]);
 
+  useEffect(() => {
+    if (content.current) {
+      const gesture = createGesture({
+        el: content.current,
+        threshold: 0,
+        onStart,
+        gestureName: "double-click",
+      });
+
+      gesture.enable();
+    }
+  });
+
   return (
     <IonPage>
-      <IonContent>
-        <IonFab edge={true} slot="fixed" vertical="bottom" horizontal="end">
-          <IonFabButton color="dark" size="small">
-            <IonIcon icon={chevronUpCircle} />
-          </IonFabButton>
-          <IonFabList side="top">
-            <IonFabButton onClick={() => history.push("/chapters-index")}>
-              <IonIcon icon={listOutline} />
+      <IonContent ref={content}>
+        {isFabButtonShown === true ? (
+          <IonFab edge={false} slot="fixed" vertical="bottom" horizontal="end">
+            <IonFabButton color="dark" size="small">
+              <IonIcon icon={chevronUpCircle} />
             </IonFabButton>
-            <IonFabButton onClick={() => history.push("/search")}>
-              <IonIcon icon={searchOutline} />
-            </IonFabButton>
-          </IonFabList>
-        </IonFab>
+            <IonFabList side="top">
+              <IonFabButton onClick={() => history.push("/chapters-index")}>
+                <IonIcon icon={listOutline} />
+              </IonFabButton>
+              <IonFabButton onClick={() => history.push("/search")}>
+                <IonIcon icon={searchOutline} />
+              </IonFabButton>
+            </IonFabList>
+          </IonFab>
+        ) : null}
         <Suspense fallback={<SpinnerWrapper />}>
           <LazyCarouselWrapper />
         </Suspense>
